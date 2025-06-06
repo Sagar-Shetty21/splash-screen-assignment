e99f75c694a6c77a407b66808bb4992447962d67
sagar-shetty21 <5agarm5hetty05@gmail.com>
2025-06-06 12:30:36 +0530

    text animation added

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 import { gsap } from "gsap";
 
-gsap.from("#splash", { opacity: 0, y: 100, duration: 1.5 });
-// gsap.to("#splash", { opacity: 0, y: -100, duration: 1.5, delay: 2 });
+gsap.fromTo(
+    "#splash-text",
+    {
+        opacity: 0,
+        y: 20,
+    },
+    {
+        delay: 1, // wait 1 second before starting
+        opacity: 1, // fade in
+        y: 0, // move up slightly to center
+        duration: 1.5,
+        ease: "power2.out",
+        onComplete: () => {
+            // after 4 seconds from animation start, fade out
+            gsap.to("#splash-text", {
+                delay: 1.5, // 1.5s animation + 1.5s wait = 4s total
+                opacity: 0,
+                duration: 1,
+                ease: "power2.in",
+            });
+        },
+    }
+);
